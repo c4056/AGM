d68156671b5939c7798ce401fceea4dc99c4ecbe
c4056 <c4056@mail.nknu.edu.tw>
2024-09-17 14:48:37 +0800

    Update script.js

--- script.js
+++ script.js
@@ -19,7 +19,8 @@ document.addEventListener('DOMContentLoaded', () => {
     document.getElementById('backToTop').addEventListener('click', (e) => {
         e.preventDefault();
         window.scrollTo({
-            top: 0,
+             top: 0, 
+            left: 0,
             behavior: 'smooth'
         });
     });
